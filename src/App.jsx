c42fea9c8fb1017e1fdf00684d35ddeeb37b2dad
chadcito <145295470+chadcito@users.noreply.github.com>
2024-09-16 14:42:44 -0600

    added root layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RoomPage from "./pages/RoomPage";
+import MainPage from "./pages/MainPage";
+import Root from "./Layouts/Root";
 
 const App = () => {
 
   const router = createBrowserRouter([
     {
-      path: '/:roomId',
-      element: <RoomPage />
+      path: '/',
+      element: <Root />,
+      children: [
+        {
+          index: true,
+          element: <MainPage />
+        },
+        {
+          path: '/rooms/:roomId',
+          element: <RoomPage />
+        },
+      ]
+      
     },
   ]);
 
