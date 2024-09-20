d07030303ca1893fb32696389fd98dccec801304
chadcito <145295470+chadcito@users.noreply.github.com>
2024-09-19 23:05:22 -0600

    created room service

--- src/pages/RoomPage.jsx
+++ src/pages/RoomPage.jsx
@@ -1,13 +1,27 @@
 import { PreJoin } from '@livekit/components-react';
 import LiveKitConference from '../components/LiveKitConference';
 import '@livekit/components-styles';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { generateRoom } from '../services/roomService';
 
 const RoomPage = () => {
   const [username, setUserName] = useState('');
   const handleSubmit = (values) => {
     setUserName(values.username);
   };
+
+  const params = useParams();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await generateRoom(params.roomId);
+      } catch (error) {
+        console.error('Error generating room ', error);
+      }
+    })();
+  }, [params.roomId]);
 
   return (
     <>
